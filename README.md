699aa1b94c7004787c1f6b88016c1c9b04de8b6a
Kilian Brendel <kilian.brendel@posteo.de>
2020-04-14 10:48:19 +0200

    Summing up regions per country

--- README.md
+++ README.md
@@ -24,4 +24,6 @@ To generate figures, run the `time_series.py` script with one or more country na
 python3 time_series.py US "United Kingdom" Germany China
 ```
 
+Since there is no data for China as a whole available, the sum of all available regions in China will be computed and shown.  For some countries, e.g. France, data for the whole country and for individual regions is available.  It is possible to refer to the sum of all regional data for France as `"France ALL"` (it's important to include the quotation marks on the command line).  By comparing `France` and `"France ALL"` you will see that the numbers are very different.  For the case of France, this is because `France` apparently refers to mainland France, while the regions refer to French islands such as Guadeloupe or Martinique.  Whenever you are in doubt about what is shown in the figures, it is probably a good idea to look at the original data tables.
+
 The figures are saved to disk as png files in a subdirectory named `figures`.  If supported by the matplotlib backend, they are also displayed on screen, hit `Ctrl+C` on the terminal to close all at once.
--- time_series.py
+++ time_series.py
@@ -6,6 +6,7 @@ import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from copy import deepcopy
 
 # Path for saving output images, set to None if you do not want to save
 save_dir = "figures"
@@ -57,6 +58,7 @@ class Entry:
 
 def load_data(filename):
     countries = {}
+    computed = {}
     values = {}
     with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -66,7 +68,32 @@ def load_data(filename):
         for row in reader:
             c = Country(tuple(row[:4]))
             countries[c.full_name] = c
-            values[c.full_name] = tuple(int(item) for item in row[4:])
+            row_data = np.array([int(item) for item in row[4:]])
+            values[c.full_name] = row_data
+            # Compute sums for states where individual provinces are listed
+            if c.province:
+                placeholder = c.name + " ALL"
+                if placeholder in computed:
+                    print(placeholder, "-- ADDING", c.province)
+                    print("  Sum before: ", values[placeholder][-1], end='')
+                    values[placeholder] += row_data
+                    print("  Sum after: ", values[placeholder][-1])
+                else:
+                    # Create new synthetic entry; copy Country and data row to
+                    # ensure that original entry is not affected!
+                    c = deepcopy(c)
+                    row_data = deepcopy(row_data)
+                    c.full_name = c.name + " (summed from region data)"
+                    computed[placeholder] = c
+                    values[placeholder] = row_data
+    for placeholder, c in computed.items():
+        # if there is no name collision, remove the " ALL" suffix from the name
+        name = placeholder[:-4]
+        if not name in countries:
+            countries[name] = computed[placeholder]
+            values[name] = values[placeholder]
+        else:
+            countries[placeholder] = computed[placeholder]
     return Dataset(countries, dates, values)
 
 
@@ -95,11 +122,12 @@ def main(countries):
 
     for country in countries:
         if not country in all_countries:
-            # Try to find matching state/province
-            country = next((c.full_name for c in all_countries.values() if c.province == country), None)
-            if country is None:
+            # Try to expand name if there is a matching state/province
+            selected = next((c.full_name for c in all_countries.values() if c.province == country), None)
+            if selected is None:
                 print("Country '{}' not found.".format(country))
                 continue
+            country = selected
         fig, ax = plt.subplots()
         title = "{} up to {}".format(all_countries[country].full_name, date_end)
         ax.set(title=title, **options)
@@ -110,7 +138,7 @@ def main(countries):
         for entry in entries:
             dataset = datasets[entry.tag]
             data_x = np.array(dataset.dates) + dt.timedelta(entry.date_offset)
-            data_y = np.array(dataset.values[country]) * entry.scale
+            data_y = dataset.values[country] * entry.scale
             ax.scatter(data_x, data_y, label=entry.label, color=entry.color)
         legend = ax.legend(loc='upper left', shadow=True, fontsize='x-large')
         legend.get_frame().set_facecolor('C4')
